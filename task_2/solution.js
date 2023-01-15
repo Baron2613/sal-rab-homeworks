36e8fb0a747d7110310580168f6eddb35ec2dadb
Artem S <sam2600@mail.ru>
2023-01-15 18:32:35 +0500

    v.4

--- task_2/solution.js
+++ task_2/solution.js
@@ -5,7 +5,15 @@ function calcShipping(sum, min, shipping) {
 
     // Задание №2.1. Рассчитать доставку
     let shippingSum;
-    
+     if(productsSum == 0){
+        shippingSum = 0
+     }
+     if(productsSum >= freeShippingMinSum){
+        shippingSum = 0
+     }
+     if(productsSum > 0 && productsSum < freeShippingMinSum){
+        shippingSum = shipping
+     }
 
     // создайте переменную shippingSum
 
@@ -29,6 +37,12 @@ function calcDiscount(sum, min, discount) {
     let discountPart = discount; // величина скидки в процентах
 
     // Задание №2.2. Рассчитать скидку
+    let discountSum;
+    if(productsSum >= discountMinSum){
+        discountSum = sum / 100 * discount
+    }else{
+        discountSum = 0
+    }
 
     // создайте переменную discountSum
 
@@ -46,6 +60,8 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     let discountSum = calcDiscount(sum, discountMinSum, discountPart);
 
     // Задача №2.3. Рассчитать скидки и доставку в корзине
+    let totalSum = sum - discountSum;
+    
 
     // создайте переменную totalSum
 
@@ -53,6 +69,11 @@ function calcInvoice({sum, discountMinSum, discountPart, shippingFreeMinSum, shi
     // уменьшите totalSum на discountSum
 
     let shippingSum = calcShipping(totalSum, shippingFreeMinSum, shippingPrice); // не изменяйте эту строку!!!
+    totalSum + shippingSum;
+    let freeShipping = shippingSum == 0;
+      
+
+
 
     // прибавьте к totalSum значение shippingSum
 
